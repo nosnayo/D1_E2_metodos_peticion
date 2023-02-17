6fa2ee9a55e5373c4578e00a2dcbd82bdad390f2
nosnayo <nelson.osnayo.c@gmail.com>
2023-02-17 09:43:57 -0300

    avance ejercicio rutas

--- db.js
+++ db.js
@@ -19,20 +19,55 @@ client.connect((err) => {
 
 const traerConductores = () =>{
   //Esto tiene un Callback
-  client.query("SELECT * FROM conductores;", (err, result) => {
-    if(err){
-      return "Ha ocurrido un error."
-    }
-    //console.log(result) //La propiedad rows tiene los valores
-    console.table(result.rows)
-    client.end();
+  //Hacer un query es asincrono en su return.
+  return new Promise((resolve, reject) =>{
+    client.query("SELECT * FROM conductores;", (err, result) => {
+      if(err){
+        //return "Ha ocurrido un error."
+        reject(err)
+      }
+      //console.log(result) //La propiedad rows tiene los valores
+      //console.table(result.rows)
+      client.end();
+      //return result.rows
+      resolve(result.rows)
+    })
+  })  
+}
+
+const traerVehiculos = () => {
+  return new Promise((resolve, reject) => {
+    client.query("SELECT CONCAT(marca, ' - ',patente) vehiculo FROM automoviles;", (err, result) => {
+      if(err){
+          reject(err)
+      }
+      resolve(result.rows)
+    })
   })
 }
+
+const traerConductoresSinVehiculo = (edad) => {
+  return new Promise((resolve, reject) => {
+    //Conductores que de sierta edad no tiene vehiculo
+    client.query(`select nombre, edad from conductores c
+                    left join automoviles a
+                    ON c.nombre = a.nombre_conductor
+                    where edad <= ${edad} and marca is null`, (err, result) => {
+      if(err){
+         return reject(err)
+      }
+      resolve(result.rows)
+    })
+  })
+}
+
 
 //LLAMADA DIRECTA DE ESTE ARCHIVO
 //traerconductores();
 
 //EXPORTAR CON ECMAScript5
 module.exports = {
-  traerConductores
+  traerConductores,
+  traerVehiculos,
+  traerConductoresSinVehiculo
 }
--- index.js
+++ index.js
@@ -1,20 +1,62 @@
 const express = require('express')
 const app = express()
-const { traerConductores } = require('./db.js')
+const { traerConductores, traerVehiculos, traerConductoresSinVehiculo } = require('./db.js')
 
 app.get('/', function (req, res) {
   res.send('Ejercicio 2 ')
 })
 
 //RETORNA LA LISTA DE TODOS LOS CONDUCTORES
-app.get('/conductores', function (req, res) {
-  let conductores = traerConductores();
+app.get('/conductores', async function (req, res) {
+  let conductores = await traerConductores();
   if(!conductores){
-    res.send('no existen conductes en la BD.')
+    res.send('no existen conductores en la BD.')
   }else{
     let acumulador = "<ol>"
+    conductores.forEach(conductor => {
+      //console.log(conductor)
+      acumulador+= `<li>${conductor.nombre}</li>`
+    });
+    acumulador+="</ol>"
+    res.send(acumulador);
   }
-  res.send('Ruta Conductores')
+  //res.send('RUTA CONDUCTORES')
+})
+
+//RETORNA LA LISTA DE TODOS LOS VEHICULOS
+app.get('/vehiculos', async function(req, res) {
+  traerVehiculos().then(vehiculos => {
+      let acumulador = "<ol>"
+      vehiculos.forEach(vehiculo => {
+          console.log(vehiculo)
+          acumulador+= `<li>${vehiculo.vehiculo}</li>`
+      });
+      acumulador+="</ol>"
+      res.send(acumulador);
+
+  }).catch(error => {
+      res.send('no existen vehiculos en la BD.')
+  })
+})
+
+app.get('/conductoressinauto', async function(req, res) {
+  let edad = req.query.edad || 99;  //Si no hay nada es 99
+  traerConductoresSinVehiculo(edad).then(listado => {
+    if(listado.length == 0){  //ejemplo: ?edad=21
+        return res.send(`<h1>No se encontraron usuarios sin autos con una edad inferior o igual a ${edad}</h1>`)
+    }
+    let acumulador = "<ol>"
+    listado.forEach(conductor => {
+        console.log(conductor )
+        acumulador+= `<li>Nombre: ${conductor.nombre} - Edad: ${conductor.edad}</li>`
+    });
+    acumulador+="</ol>"
+    res.send(acumulador);
+
+  }).catch(error => {
+      console.log(error)
+      res.status(500).send("Error interno del servidor.");
+  })
 })
 
 //SIN ECMASCcript6
